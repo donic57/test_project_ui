bd69d2baa1cd69dd0206be96d4babf494d42f1a0
donic57 <lyadov_l@list.ru>
2025-07-30 09:57:19 +0300

    правки по классу SalePage

--- pages/sale_page.py
+++ pages/sale_page.py
@@ -7,6 +7,10 @@ from selenium.webdriver import ActionChains
 
 class SalePage(BasePage):
 
+    quantity_items = None
+    number_item_products_one_page = None
+    text_gear = None
+
     page_url = '/sale.html'
 
     def check_text_page_sale(self, text1, text2):
@@ -18,37 +22,45 @@ class SalePage(BasePage):
             loc.text_categories_menu_loc).get_attribute('innerText')
         assert text_categories_menu == text2
 
-    def check_quantity_items_in_pages(self, text):
+    def page_women_sale(self, text):
         link_women_deals = self.find(loc.link_women_deals_loc)
         self.driver.execute_script('arguments[0].scrollIntoView();', link_women_deals)
         link_women_deals.click()
         text_page_women_sale = self.find(loc.text_page_women_sale_loc).text
         assert text_page_women_sale == text
-        quantity_items = int(self.find(loc.quantity_items_loc).text)
+
+    def check_quantity_items_in_pages(self):
+        self.quantity_items = int(self.find(loc.quantity_items_loc).text)
         item_products_one_page = self.find_all(loc.item_products_one_page_loc)
-        number_item_products_one_page = int(len(item_products_one_page))
+        self.number_item_products_one_page = int(len(item_products_one_page))
         self.driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
         button_next = self.find(loc.button_next_loc)
         self.driver.execute_script('arguments[0].scrollIntoView();', button_next)
         button_next.click()
+
+    def comparison_item_pages(self):
         WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(loc.product_item_link_loc))
         item_products_two_page = self.find_all(loc.product_item_link_loc)
         number_item_products_two_page = int(len(item_products_two_page))
-        assert quantity_items == number_item_products_one_page + number_item_products_two_page
+        assert self.quantity_items == self.number_item_products_one_page + number_item_products_two_page
 
-    def check_page_gear(self, text1, text2):
+    def click_page_gear(self):
         self.driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
         link_gear_steals = self.find(loc.link_gear_steals_loc)
         self.driver.execute_script('arguments[0].scrollIntoView();', link_gear_steals)
         link_gear_steals.click()
-        text_gear = self.find(loc.base_loc).text
-        assert text_gear == text1
+        self.text_gear = self.find(loc.base_loc).text
+
+    def check_in_text_page_gear(self, text1, text2):
+        assert self.text_gear == text1
         WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located(loc.sidebar_sidebar_main_loc))
         text_menu = self.find(
             loc.text_menu_loc).get_attribute('innerText')
         assert text_menu == text2
+
+    def click_page_bags(self):
         menu_gear = self.find(loc.menu_gear_loc)
         actions = ActionChains(self.driver)
         actions.move_to_element(menu_gear).perform()
@@ -57,6 +69,9 @@ class SalePage(BasePage):
         actions.move_to_element(link_bags).click().perform()
         text_page_bags = self.find(loc.base_loc).text
         assert text_link_page_bags == text_page_bags
+
+    def click_page_fitness_equipment(self):
+        actions = ActionChains(self.driver)
         menu_gear = self.find(loc.menu_gear_loc)
         actions.move_to_element(menu_gear).perform()
         link_fitness_equipment = self.find(loc.link_fitness_equipment_loc)
@@ -64,6 +79,9 @@ class SalePage(BasePage):
         actions.move_to_element(link_fitness_equipment).click().perform()
         text_page_fitness_equipment = self.find(loc.base_loc).text
         assert text_link_fitness_equipment == text_page_fitness_equipment
+
+    def click_page_watches(self):
+        actions = ActionChains(self.driver)
         menu_gear = self.find(loc.menu_gear_loc)
         actions.move_to_element(menu_gear).perform()
         link_watches = self.find(loc.link_watches_loc)
--- tests/test_page_sale.py
+++ tests/test_page_sale.py
@@ -11,10 +11,16 @@ def test_text_page_sale(sale_page):
 
 def test_quantity_items_in_pages(sale_page):
     sale_page.open_page()
-    sale_page.check_quantity_items_in_pages('Women Sale')
+    sale_page.page_women_sale('Women Sale')
+    sale_page.check_quantity_items_in_pages()
+    sale_page.comparison_item_pages()
 
 
 def test_page_gear(sale_page):
     sale_page.open_page()
-    sale_page.check_page_gear("Gear", "Shop By\nCategory\nBags 14\n"
+    sale_page.click_page_gear()
+    sale_page.heck_in_text_page_gear("Gear", "Shop By\nCategory\nBags 14\n"
                              "Fitness Equipment 11\nWatches 9\nBags\nFitness Equipment\nWatches")
+    sale_page.click_page_bags()
+    sale_page.click_page_fitness_equipment()
+    sale_page.click_page_watches()
